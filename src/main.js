008ec47afe644a33efe9e1e51244a441e7330a75
Gonzalo Cuadra <gonzalo.cuadra@frogmi.com>
2019-08-01 11:30:41 -0400

    Add vue-bootstrap

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import App from './App.vue'
 import ErrorMessage from './components/ErrorMessage.vue'
 import UserList from './components/UserList.vue'
 import NewUser from './components/NewUser.vue'
 
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 Vue.component('error-message', ErrorMessage)
 Vue.component('user-list', UserList)
